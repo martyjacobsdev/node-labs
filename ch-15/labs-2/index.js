7c6e33085031aca161857d28b4db27ec70f451f9
martyjacobsdev <martyjacobs2009@hotmail.com>
2020-10-01 01:15:17 +1000

    spawn lab completed

--- ch-15/labs-2/index.js
+++ ch-15/labs-2/index.js
@@ -1,9 +1,15 @@
 'use strict'
 
 const { spawn } = require('child_process')
+const { stdin } = require('process')
 
 function exercise (command, args) {
-  return spawn(command, args)
+
+
+//If a child process is started with the stdio option set to 
+//['pipe', 'ignore', 'inherit'], how will the I/O of the child process behave?
+
+  return spawn(command, args, { stdio: ['ignore', 'inherit', 'pipe'] })
 }
 
 module.exports = exercise
