3283f62c6263741c08bb2bd1c28c1dfb21c044c6
martyjacobsdev <martyjacobs2009@hotmail.com>
2020-10-01 11:32:34 +1000

    more done

--- ch-15/labs-1/index.js
+++ ch-15/labs-1/index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const { env } = require('process')
+
 function exercise (myEnvVar) {
   // TODO return a child process with
   // a single environment variable set 
@@ -8,7 +10,10 @@ function exercise (myEnvVar) {
   // be the value of the myEnvVar parameter 
   // passed to this exercise function
 
-  return require('child_process').spawnSync(process.argv[0], ['child.js'])
+  //learned::  that the function was already have done -- returning a child process was done 
+  //the options was to include an object , referencing env - which is key/value pairs 
+
+  return require('child_process').spawnSync(process.argv[0], ['child.js'], { env : { MY_ENV_VAR : myEnvVar } })
 }
 
 module.exports = exercise
